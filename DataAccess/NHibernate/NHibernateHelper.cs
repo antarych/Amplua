f4650effcc194fe3606c64e417c2e6245b8c3aa9
antarych <lizardia@mail.ru>
2017-01-04 20:27:21 +0300

    hibernate configuration to xml

--- DataAccess/NHibernate/NHibernateHelper.cs
+++ DataAccess/NHibernate/NHibernateHelper.cs
@@ -1,13 +1,10 @@
 ﻿using NHibernate;
-using System.Reflection;
-using System.Threading;
 using NHibernate.Cfg;
 using NHibernate.Cfg.MappingSchema;
-using NHibernate.Dialect;
 using NHibernate.Mapping.ByCode;
 using NHibernate.Tool.hbm2ddl;
-using System.Web;
 using DataAccess.Mappings;
+
 
 namespace DataAccess
 {
@@ -15,11 +12,7 @@ namespace DataAccess
     {
         public static ISession OpenSession()
         {
-            var cfg = new Configuration()
-            .DataBaseIntegration(db => {
-                db.ConnectionString = @"Server=localhost;Database=Amplua;User ID=newuser;Password=password;";
-                db.Dialect<MySQL5InnoDBDialect>();
-            });
+            var cfg = new Configuration().Configure();
             var mapper = new ModelMapper();
             mapper.AddMapping<UserMap>();
             HbmMapping mapping = mapper.CompileMappingForAllExplicitlyAddedEntities();
